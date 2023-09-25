a2ba52c0217b26f6360c885f09dc8ad413702b85
Kiril Kartunov <kiril.kartunov@gmail.com>
2023-09-25 19:53:03 +0300

    Use npm instead of yarn

--- src/config/Config.ts
+++ src/config/Config.ts
@@ -3,11 +3,7 @@ dotenv.config({ path: __dirname + `/../../.env.${process.env.NODE_ENV}` });
 
 const config = {
     port: process.env.PORT,
-    dbUrl: process.env.DB_URL,
-    dbHost: process.env.DB_HOST ?? 'test_host',
-    dbUser: process.env.DB_USER ?? 'test_user',
-    dbPassword: process.env.DB_PASSWORD ?? 'test_password',
-    dbName: process.env.DB_NAME ?? 'test_db',
+    dbUrl: process.env.TC_SKILLS_DATABASE_URL,
 };
 
 export default config;
