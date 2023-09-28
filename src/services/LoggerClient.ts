fe46c1f9650db7c191555b20fbbbae50941e14c4
Kiril Kartunov <kiril.kartunov@gmail.com>
2023-09-28 06:55:04 +0300

    fixes lint errors

--- src/services/LoggerClient.ts
+++ src/services/LoggerClient.ts
@@ -14,20 +14,20 @@ export class LoggerClient {
         if (process.env.NODE_ENV === 'local') {
             this.logger.add(
                 new transports.Console({
-                    format: format.combine(label({ label: "StandardizedSkillsApi\t" }), format.colorize(), format.simple()),
+                    format: format.combine(label({ label: 'StandardizedSkillsApi\t' }), format.colorize(), format.simple()),
                 }),
             );
         } else {
             this.logger.add(
                 new transports.Console({
                     format: combine(
-                        label({ label: "StandardizedSkillsApi" }),
+                        label({ label: 'StandardizedSkillsApi' }),
                         timestamp(),
                         json(),
                         format.printf((info) => `[${info.label}] [${info.timestamp}] - [${info.level.toUpperCase()}] : ${info.message}`)
                     )
                 })
-            )
+            );
         }
     }
 
