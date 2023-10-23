f9ef2ab04b0b8094b06cf7c044c74a45266e06fe
Vasilica Olariu <olariu.vasilica@gmail.com>
2023-10-23 11:47:42 +0300

    Update user-skills-helper

--- src/utils/user-skills-helper.ts
+++ src/utils/user-skills-helper.ts
@@ -12,29 +12,25 @@ export function getOrderBy(query: GetUserSkillsQueryDto) {
     let order: Order = [];
     let sortBy = query.sortBy;
 
-    if (query.sortBy) {
-        switch (query.sortBy) {
-            case 'level':
-                order = [
-                    [
-                        { model: UserSkill, as: 'user_skills' },
-                        { model: UserSkillLevel, as: 'user_skill_level' },
-                        'name',
-                        query.sortOrder || 'asc',
-                    ],
-                ];
-            case 'category':
-                order = [[{ model: SkillCategory, as: 'category' }, 'name', query.sortOrder || 'asc']];
-            case 'level':
-            case 'category':
-                // secondary sort by skill name
-                order.push(['name', 'asc']);
-                // clear sort criteria as it is unknown to db
-                sortBy = undefined;
-                break;
-            default:
-                break;
-        }
+    if (query.sortBy === 'level') {
+        order = [
+            [
+                { model: UserSkill, as: 'user_skills' },
+                { model: UserSkillLevel, as: 'user_skill_level' },
+                'name',
+                query.sortOrder || 'asc',
+            ],
+        ];
+    }
+    if (query.sortBy === 'category') {
+        order = [[{ model: SkillCategory, as: 'category' }, 'name', query.sortOrder || 'asc']];
+    }
+
+    if (query.sortBy === 'level' || query.sortBy === 'category') {
+        // secondary sort by skill name
+        order.push(['name', 'asc']);
+        // clear sort criteria as it is unknown to db
+        sortBy = undefined;
     }
 
     return { order, sortBy };
