dc5fddae92040d06418f1dc18fc2823bb4bb318a
Aranyajit <aranyajit.sarkar@topcoder.com>
2023-11-02 01:56:21 +0530

    create script to delete skills and categories outside original

---
+++ src/scripts/delete-test-skills.ts
@@ -0,0 +1,86 @@
+import { map, uniq } from 'lodash';
+import * as allSkills from './all_skills.json';
+import { Skill, SkillCategory } from '../db';
+import { Op } from 'sequelize';
+import { LoggerClient } from '../utils/LoggerClient';
+
+const logger = new LoggerClient('delete_test-skills');
+
+const destroySkillsOutsideOriginal = async () => {
+    logger.info('Deleting test skills outside of the original skills');
+
+    const originalSkillIds = map(
+        allSkills as {
+            id: string;
+            name: string;
+            description: string | null;
+            category_id: string;
+            created_at: string | null;
+            updated_at: string | null;
+            deleted_at: string | null;
+        }[],
+        (skill) => skill.id,
+    );
+    logger.info(`The number of original skills in the DB: ${originalSkillIds.length}`);
+
+    let numOfSkills = await Skill.count({
+        paranoid: false,
+    });
+    logger.info(`Total skills present in the DB: ${numOfSkills}`);
+
+    // hard delete skills from the DB as they are test skills
+    const deletedSkills = await Skill.destroy({
+        where: {
+            id: {
+                [Op.notIn]: originalSkillIds,
+            },
+        },
+        force: true,
+    });
+    logger.info(`Number of skills deleted from DB: ${deletedSkills}`);
+
+    numOfSkills = await Skill.count({
+        paranoid: false,
+    });
+    logger.info(`Deletion successful, number of skills left in the DB: ${numOfSkills}`);
+};
+
+const destroyCategoriesOutsideOriginal = async () => {
+    logger.info('Deleting test categories outside of the original skills');
+
+    const originalCategoryIds = uniq(
+        map(
+            allSkills as {
+                id: string;
+                name: string;
+                description: string | null;
+                category_id: string;
+                created_at: string | null;
+                updated_at: string | null;
+                deleted_at: string | null;
+            }[],
+            (skill) => skill.category_id,
+        ),
+    );
+    logger.info(`The number of original categories in the DB: ${originalCategoryIds.length}`);
+
+    let numOfCategories = await SkillCategory.count();
+    logger.info(`Total categories present in the DB: ${numOfCategories}`);
+
+    // hard delete categories from the DB as they are test categories
+    const deletedCategories = await SkillCategory.destroy({
+        where: {
+            id: {
+                [Op.notIn]: originalCategoryIds,
+            },
+        },
+    });
+    logger.info(`Number of categories deleted from DB: ${deletedCategories}`);
+
+    numOfCategories = await SkillCategory.count();
+    logger.info(`Deletion successful, total categories present in the DB: ${numOfCategories}`);
+};
+
+destroySkillsOutsideOriginal()
+    .then(() => destroyCategoriesOutsideOriginal())
+    .catch(logger.error);
