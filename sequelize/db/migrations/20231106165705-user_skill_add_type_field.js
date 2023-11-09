0bacb636147236a8e02b2dcbc435cd01a78ab672
Vasilica Olariu <olariu.vasilica@gmail.com>
2023-11-09 12:18:22 +0200

    TSJR-211 - update migration files

--- sequelize/db/migrations/20231106165705-user_skill_add_type_field.js
+++ sequelize/db/migrations/20231106165705-user_skill_add_type_field.js
@@ -3,24 +3,24 @@
 const { DataTypes } = require('sequelize');
 
 /**
- * Migration to add a new `user_skill_type` field to the `user_skill` table
+ * Migration to add a new `user_skill_display_mode` field to the `user_skill` table
  */
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
     async up(queryInterface) {
-        await queryInterface.addColumn('user_skill', 'user_skill_type_id', {
+        await queryInterface.addColumn('user_skill', 'user_skill_display_mode_id', {
             type: DataTypes.UUID,
             allowNull: false,
             defaultValue: '1555aa05-a764-4f0b-b3e0-51b824382abb',
             references: {
-                model: 'user_skill_type',
+                model: 'user_skill_display_mode',
                 key: 'id',
             },
-            unique: 'uniq_user_skill_type',
+            unique: 'uniq_user_skill_display_mode',
         });
     },
 
     async down(queryInterface) {
-        await queryInterface.removeColumn('user_skill', 'user_skill_type_id');
+        await queryInterface.removeColumn('user_skill', 'user_skill_display_mode_id');
     },
 };
