8f695d4e08ceba28f4e0c299d5459cfc2b9f639f
Justin Gasper <jmgasper@gmail.com>
2026-01-07 14:42:35 +1100

    Expose skill-events

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { configuration } from './config';
 import { PrismaModule } from './prisma/prisma.module';
 import { HealthModule } from './modules/health/health.module';
 import { SkillCategoriesModule } from './modules/skill-categories/skill-categories.module';
+import { SkillEventsModule } from './modules/skill-events/skill-events.module';
 import { SkillsModule } from './modules/skills/skills.module';
 import { UserSkillsModule } from './modules/user-skills/user-skills.module';
 import { WorkSkillsModule } from './modules/work-skills/work-skills.module';
@@ -19,6 +20,7 @@ import { WorkSkillsModule } from './modules/work-skills/work-skills.module';
     PrismaModule,
     HealthModule,
     SkillsModule,
+    SkillEventsModule,
     SkillCategoriesModule,
     UserSkillsModule,
     WorkSkillsModule,
